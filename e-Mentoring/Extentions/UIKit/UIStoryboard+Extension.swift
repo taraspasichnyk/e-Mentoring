29e4ed4899b0dee2a361c54ebb59ecb6696000c4
Taras Pasichnyk <dev.taras.pasichnyk@gmail.com>
2017-06-30 23:50:28 +0300

    Minor changes

---
+++ e-Mentoring/Extentions/UIKit/UIStoryboard+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIStoryboard+Extension.swift
+//  e-Mentoring
+//
+//  Created by Taras Pasichnyk on 6/30/17.
+//  Copyright © 2017 Taras Pasichnyk. All rights reserved.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    public class var swiper: UIStoryboard {
+        return UIStoryboard(name: "Swiper", bundle: nil)
+    }
+    
+    public class var login: UIStoryboard {
+        return UIStoryboard(name: "Login", bundle: nil)
+    }
+}
